678732c7046d17da732335cf98e304f635c516c8
iveth43 <iveth.contreras@jpschools.org>
2026-06-22 16:28:58 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,18 +20,28 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle; // variable to hold a single circle when creating circles / iterating
+        var circles = []; // variable to store all circles in an array    
 
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+         circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+         physikz.addRandomVelocity(circle, canvas, 5, 5);
+         view.addChild(circle);
+        circles.push(circle);
+    }
 
 
         // TODO 3 : Call the drawCircle() function
-
+       
 
 
         // TODO 7 : Use a loop to create multiple circles
+        for (var i = 0; i < 25; i++) {
+        drawCircle()
+}
 
 
 
@@ -47,15 +57,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
+        
 
-            
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
+         
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
+           for (var i = 0; i < circles.lenght; i++) {
+          physikz.updatePosition(circles[i]);
+          game.checkCirclePosition(circles[i]);
+           }
+         }
             
-        }
+        
     
         /* 
         This Function should check the position of a circle that is passed to the 
@@ -70,8 +84,22 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+           
+    
+// Left edge → right edge
+if (circle.x + circle.radius < 0) {
+    circle.x = canvas.width + circle.radius;
+}
 
+// Top edge → bottom edge
+if (circle.y + circle.radius < 0) {
+    circle.y = canvas.height + circle.radius;
+}
+
+// Bottom edge → top edge
+if (circle.y - circle.radius > canvas.height) {
+    circle.y = -circle.radius;
+}
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
